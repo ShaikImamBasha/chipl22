a5c3d3cdaa3898698db1f6004f0061bd882bd708
ShaikImamBasha <shaikimambasha1437@gmail.com>
2024-12-04 16:43:50 +0530

    Create Employee.java

---
+++ q1/Employee.java
@@ -0,0 +1,21 @@
+package com.codinghub;
+
+public class Employee11 {
+	int id;
+	String name;
+	String dept;
+	double salary;
+	public Employee11(int id, String name, String dept, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dept = dept;
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Employee11 [id=" + id + ", name=" + name + ", dept=" + dept + ", salary=" + salary + "]";
+	}
+	
+
+}
